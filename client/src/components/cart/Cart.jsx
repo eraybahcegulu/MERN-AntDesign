e52303103980e29f682fa30151ff29cfbb2e86ec
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-27 14:20:43 +0300

    card design update

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,54 +1,41 @@
-import { Card, Image, Row, Col } from 'antd';
+import { Card, Image } from "antd";
 
 const Cart = () => {
   return (
-    <div className="border-black border-2 h-full flex flex-col">
-      <ul className="p-5 flex flex-col overflow-y-auto gap-4">
-        <li className="border-2">
-          <div className="flex justify-between items-center p-2 ">
-            <div className="flex flex-col border-2">
-            <Image
-                width={64}
-                height={64}
-                src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
-                alt=""
-              />
-              <div className="flex flex-row gap-2 justify-center">
-                <span>aqwdqdwdqw</span>
-                <span>20$</span>
+    <div className="h-full flex flex-col">
+      <div className="text-center border border-b-0">
+       <strong>CART</strong> 
+      </div>
+      <Card className=" h-full overflow-y-auto rounded-none">
+        <div>
+          <ul className=" flex flex-col overflow-y-auto gap-4">
+            <li className="border">
+              <div className="flex justify-between items-center p-4 ">
+                <div className="flex flex-col border p-2">
+                  <Image
+                    width={64}
+                    height={64}
+                    src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
+                    alt=""
+                  />
+                  <div className="flex flex-row gap-2 justify-center">
+                    <span>test</span>
+                    <span>20$</span>
+                  </div>
+                </div>
+                <div>
+                  <span>2x</span>
+                </div>
               </div>
-            </div>
-            <div>
-              <span>2x</span>
-            </div>
-          </div>
-        </li>
+            </li>
 
-        <li className="border-2">
-          <div className="flex justify-between items-center p-2 ">
-            <div className="flex flex-col border-2">
-            <Image
-                width={64}
-                height={64}
-                src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
-                alt=""
-              />
-              <div className="flex flex-row gap-2 justify-center">
-                <span>aqwdqdwdqw</span>
-                <span>20$</span>
-              </div>
-            </div>
-            <div>
-              <span>2x</span>
-            </div>
-          </div>
-        </li>
-
-        
-      </ul>
+            
+          </ul>
+        </div>
+      </Card>
 
       <div className="cart-totals mt-auto">
-        <div className="subtotal  border-t p-2">
+        <div className="subtotal  p-2">
           <div className="flex justify-between">
             <span>SUBTOTAL</span>
             <span> 1 $</span>
@@ -62,7 +49,7 @@ const Cart = () => {
           </div>
         </div>
 
-        <div className="total border-t p-2">
+        <div className="total border-t border-b p-2">
           <div className="mt-4 flex justify-between ">
             <span>TOTAL</span>
             <span> 1 $ </span>
