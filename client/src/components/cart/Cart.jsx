e87c821aacfc2b695d37573966fbc097dbd4e5d6
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-28 11:13:58 +0300

    Update Cart.jsx

--- client/src/components/cart/Cart.jsx
+++ client/src/components/cart/Cart.jsx
@@ -1,159 +1,14 @@
-import { Card, Image } from "antd";
+import { Card, Image, Button } from "antd";
 
 const Cart = () => {
   return (
     <div className="h-full flex flex-col m-4 md:m-0">
-      <div className="text-center border border-b-0">
+      <div className="text-center border border-b-0 p-2">
         <strong>CART</strong>
       </div>
       <Card className=" h-full rounded-none overflow-auto">
         <div className="">
           <ul className=" flex flex-col gap-4 ">
-            <li className="border ">
-              <div className="flex justify-between items-center  p-4 ">
-                <div className="flex flex-col text-center">
-                  <Image
-                    width={64}
-                    height={64}
-                    src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
-                    alt=""
-                  />
-
-                  <div className="flex flex-col ">
-                    <span className="max-w-[150px]">Armut</span>
-                    <span className="max-w-[150px]">
-                      {" "}
-                      <strong>1$</strong>
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="text-4xl">
-                    <strong>2</strong>
-                  </span>
-                  <span className="text-2xl">
-                    <strong>X</strong>
-                  </span>
-                </div>
-              </div>
-            </li>
-
-            <li className="border ">
-              <div className="flex justify-between items-center  p-4 ">
-                <div className="flex flex-col text-center">
-                  <Image
-                    width={64}
-                    height={64}
-                    src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
-                    alt=""
-                  />
-
-                  <div className="flex flex-col ">
-                    <span className="max-w-[150px]">Armut</span>
-                    <span className="max-w-[150px]">
-                      {" "}
-                      <strong>1$</strong>
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="text-4xl">
-                    <strong>2</strong>
-                  </span>
-                  <span className="text-2xl">
-                    <strong>X</strong>
-                  </span>
-                </div>
-              </div>
-            </li>
-
-            <li className="border ">
-              <div className="flex justify-between items-center  p-4 ">
-                <div className="flex flex-col text-center">
-                  <Image
-                    width={64}
-                    height={64}
-                    src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
-                    alt=""
-                  />
-
-                  <div className="flex flex-col ">
-                    <span className="max-w-[150px]">Armut</span>
-                    <span className="max-w-[150px]">
-                      {" "}
-                      <strong>1$</strong>
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="text-4xl">
-                    <strong>2</strong>
-                  </span>
-                  <span className="text-2xl">
-                    <strong>X</strong>
-                  </span>
-                </div>
-              </div>
-            </li>
-
-            <li className="border ">
-              <div className="flex justify-between items-center  p-4 ">
-                <div className="flex flex-col text-center">
-                  <Image
-                    width={64}
-                    height={64}
-                    src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
-                    alt=""
-                  />
-
-                  <div className="flex flex-col ">
-                    <span className="max-w-[150px]">Armut</span>
-                    <span className="max-w-[150px]">
-                      {" "}
-                      <strong>1$</strong>
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="text-4xl">
-                    <strong>2</strong>
-                  </span>
-                  <span className="text-2xl">
-                    <strong>X</strong>
-                  </span>
-                </div>
-              </div>
-            </li>
-
-            <li className="border ">
-              <div className="flex justify-between items-center  p-4 ">
-                <div className="flex flex-col text-center">
-                  <Image
-                    width={64}
-                    height={64}
-                    src="https://biosantarim.com/tema/genel/uploads/urunler/armut.png"
-                    alt=""
-                  />
-
-                  <div className="flex flex-col ">
-                    <span className="max-w-[150px]">Armut</span>
-                    <span className="max-w-[150px]">
-                      {" "}
-                      <strong>1$</strong>
-                    </span>
-                  </div>
-                </div>
-                <div>
-                  <span className="text-4xl">
-                    <strong>2</strong>
-                  </span>
-                  <span className="text-2xl">
-                    <strong>X</strong>
-                  </span>
-                </div>
-              </div>
-            </li>
-
             <li className="border ">
               <div className="flex justify-between items-center  p-4 ">
                 <div className="flex flex-col text-center">
@@ -188,29 +43,37 @@ const Cart = () => {
 
       <div className="cart-totals mt-auto ">
         <div className="subtotal  p-2">
-          <div className="flex justify-between text-blue-600">
+          <div className="flex justify-between text-blue-400">
             <span>SUBTOTAL</span>
             <span> 1$</span>
           </div>
         </div>
 
         <div className="vat  border-t p-2">
-          <div className="flex justify-between text-blue-600">
+          <div className="flex justify-between text-blue-400">
             <span>VAT</span>
             <span> +1$</span>
           </div>
         </div>
 
-        <div className="total border-t border-b p-2">
+        <div className="total border-t border-b-2 border-b-black p-2">
           <div className="mt-4 flex justify-between text-green-600 text-2xl">
             <span>
-              {" "}
               <strong>TOTAL</strong>
             </span>
             <span>
               <strong>1$</strong>
             </span>
           </div>
+        </div>
+
+        <div className="flex flex-col gap-4 mt-4">
+          <Button style={{borderRadius: '0'}} type="primary" size="large" danger>
+            <strong> CLEAR CART </strong>
+          </Button>
+          <Button style={{borderRadius: '0'}} type="primary" size="large">
+          <strong> BUY </strong>
+          </Button>
         </div>
       </div>
     </div>
