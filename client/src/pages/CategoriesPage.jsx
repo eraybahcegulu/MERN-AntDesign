f6c9f0342cb0491703e2983355ba4e7623fdefaf
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-28 22:06:02 +0300

    Create CategoriesPage.jsx

---
+++ client/src/pages/CategoriesPage.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CategoriesEdit from "../components/categories/CategoriesEdit";
+
+const CategoriesPage = () => {
+    const [categories, setCategories] = useState([]);
+  
+    useEffect(() => {
+      getCategories();
+    }, []);
+  
+    const getCategories = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/categories/get-all"
+        );
+        setCategories(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+  return (
+    <div className="h-[100vh] flex flex-col items-center justify-center">
+        <CategoriesEdit categories={categories}/>
+    </div>
+  );
+};
+
+export default CategoriesPage;
