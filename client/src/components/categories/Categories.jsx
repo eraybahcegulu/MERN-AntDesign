c57c6e796053b16e92db204865df812700271243
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-28 11:54:04 +0300

    get categories

--- client/src/components/categories/Categories.jsx
+++ client/src/components/categories/Categories.jsx
@@ -1,53 +1,28 @@
 import "./style.css";
-
-const Categories = () => {
+import { Alert } from "antd";
+const Categories = ({ categories }) => {
   return (
-    <ul className="flex flex-col gap items-center gap-4 md:mx-0 mb-4 mx-4">
-      <li className="categories-item ">
-        <span>İçecekler</span>
-      </li>
-      <li className="categories-item ">
-        <span>Yiyecekler</span>
-      </li>
-      <li className="categories-item ">
-        <span>İçecekler</span>
-      </li>
-      <li className="categories-item ">
-        <span>Yiyecekler</span>
-      </li>{" "}
-      <li className="categories-item ">
-        <span>
-          testtttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt
-        </span>
-      </li>
-      <li className="categories-item ">
-        <span>Yiyecekler</span>
-      </li>{" "}
-      <li className="categories-item ">
-        <span>İçecekler</span>
-      </li>
-      <li className="categories-item ">
-        <span>Yiyecekler</span>
-      </li>{" "}
-      <li className="categories-item ">
-        <span>İçecekler</span>
-      </li>
-      <li className="categories-item ">
-        <span>Yiyecekler</span>
-      </li>{" "}
-      <li className="categories-item ">
-        <span>İçecekler</span>
-      </li>
-      <li className="categories-item ">
-        <span>Yiyecekler</span>
-      </li>{" "}
-      <li className="categories-item ">
-        <span>İçecekler</span>
-      </li>
-      <li className="categories-item ">
-        <span>Yiyecekler</span>
-      </li>
-    </ul>
+    <div>
+      <div className="text-center border-b p-2">
+        <strong>CATEGORIES</strong>
+      </div>
+      <ul className="flex flex-col gap items-center gap-4 md:mx-0 mt-6 mb-4 mx-4 ">
+        <li className="categories-all-products ">
+          <span>ALL PRODUCTS</span>
+        </li>
+        {categories.length === 0 ? (
+          <div className="p-2">
+            <Alert message="Category not found" type="warning" showIcon />
+          </div>
+        ) : (
+          categories.map((item) => (
+            <li className="categories-item" key={item._id}>
+              <span>{item.name.toUpperCase()}</span>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
   );
 };
 
