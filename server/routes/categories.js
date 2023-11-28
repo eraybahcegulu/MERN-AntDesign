fc9895386c77c03b9c6d99a2a034f909ef3c15cb
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-28 22:06:18 +0300

    Update categories.js

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -15,7 +15,7 @@ router.post("/add", async (req, res) => {
   try {
     const newCategory = new Category(req.body);
     await newCategory.save();
-    res.status(200).json("Item added successfully.");
+    res.status(200).json("Category added successfully.");
   } catch (error) {
     res.status(500).json(error);
   }
