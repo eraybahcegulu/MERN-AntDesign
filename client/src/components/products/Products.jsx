718a8c98b386f2b26f805e20a649d51edd39b396
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-11-30 10:01:12 +0300

    Update Products.jsx

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,31 +1,17 @@
-import { Image , Alert} from "antd";
-
+import { Alert} from "antd";
+import Product from "./Product";
 const Products = ({products}) => {
+  
   return (
+    
     <div className="grid grid-cols-card gap-4 m-4 md:m-0">
       {products.length === 0 ? (
         <div className="p-2 w-[200px]">
           <Alert message="Product not found" type="warning" showIcon />
         </div>
       ) : (
-        products.map((item) => (
-          <div key={item._id} className="product-item border hover:shadow-md cursor-pointer transition-all select-none">
-            <div className="product-img">
-              <Image
-                className="min-h-[175px] object-cover w-full border-b"
-                src={item.image}
-                alt=""
-              />
-            </div>
-            <div className="text-center p-4 flex flex-col">
-              <span className="overflow-hidden">
-                {item.name}
-              </span>
-              <span>
-                <strong>{item.price}$</strong>
-              </span>
-            </div>
-          </div>
+        products.map((product) => (
+          <Product product={product} key={product._id} />
         ))
       )}
     </div>
