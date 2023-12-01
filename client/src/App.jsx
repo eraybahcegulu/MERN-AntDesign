2ae8f198761a5dddd86d50412d6c5bce808ca878
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-01 17:13:18 +0300

    Providers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import CategoriesPage from "./pages/CategoriesPage";
+import { CategoriesProvider } from "./contexts/CategoriesContext";
+import { ProductsProvider } from "./contexts/ProductsContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/categories" element={<CategoriesPage />} />
-      </Routes>
-    </BrowserRouter>
+    <CategoriesProvider>
+      <ProductsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ProductsProvider>
+    </CategoriesProvider>
   );
 }
 
