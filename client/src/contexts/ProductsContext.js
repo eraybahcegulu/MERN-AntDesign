a68ba3a2c8c94d1b816157ac308c8d7403106f11
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-01 23:22:58 +0300

    Update ProductsContext.js

--- client/src/contexts/ProductsContext.js
+++ client/src/contexts/ProductsContext.js
@@ -1,12 +1,10 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import { Spin } from 'antd';
 
 const ProductsContext = createContext();
 const GET_PRODUCTS_API_URL = "http://localhost:5000/api/products/get-all";
 
 export const ProductsProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
   const fetchProductsData = async (url, setData) => {
@@ -15,18 +13,12 @@ export const ProductsProvider = ({ children }) => {
       setData(response.data);
     } catch (error) {
       console.error(error);
-    } finally {
-      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchProductsData(GET_PRODUCTS_API_URL, setProducts);
   }, []);
-
-  if (loading) {
-    return <Spin size="large" />;
-  }
 
   return (
     <ProductsContext.Provider
