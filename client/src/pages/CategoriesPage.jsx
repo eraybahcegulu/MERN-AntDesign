35b796e8f20422d2795b1071faadc7ef228c5e7c
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-01 07:58:32 +0300

    CategoriesContext added

--- client/src/pages/CategoriesPage.jsx
+++ client/src/pages/CategoriesPage.jsx
@@ -1,36 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 import CategoriesEdit from "../components/categories/CategoriesEdit";
+import { CategoriesProvider } from "../contexts/CategoriesContext";
 
 const CategoriesPage = () => {
-    const [categories, setCategories] = useState([]);
-  
-    useEffect(() => {
-      getCategories();
-    }, []);
-
-    const getCategories = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/categories/get-all"
-        );
-        setCategories(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    const handleCategoryDeleted = () => {
-        getCategories();
-      };
-
-      const handleCategoryUpdated = () => {
-        getCategories();
-      };
-
   return (
     <div className="h-[100vh] flex flex-col items-center justify-center">
-        <CategoriesEdit categories={categories} categoryDeleted={handleCategoryDeleted} categoryUpdated={handleCategoryUpdated}/>
+      <CategoriesProvider>
+        <CategoriesEdit />
+      </CategoriesProvider>
     </div>
   );
 };
