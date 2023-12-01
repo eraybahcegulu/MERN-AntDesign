eb8eec7df47d074cf1b91eb8bdc4f5a651e5a581
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-01 23:22:56 +0300

    Update CategoriesContext.js

--- client/src/contexts/CategoriesContext.js
+++ client/src/contexts/CategoriesContext.js
@@ -1,12 +1,10 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-import { Spin } from 'antd';
 
 const CategoriesContext = createContext();
 const GET_CATEGORIES_API_URL = "http://localhost:5000/api/categories/get-all";
 
 export const CategoriesProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
   const [categories, setCategories] = useState([]);
 
   const fetchCategoriesData = async (url, setData) => {
@@ -15,18 +13,12 @@ export const CategoriesProvider = ({ children }) => {
       setData(response.data);
     } catch (error) {
       console.error(error);
-    } finally {
-      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchCategoriesData(GET_CATEGORIES_API_URL, setCategories);
   }, []);
-
-  if (loading) {
-    return <Spin size="large" />;
-  }
 
   return (
     <CategoriesContext.Provider
