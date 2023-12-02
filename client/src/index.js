3d2795942f84921b39d17adf9c1d1c50e24c56fd
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-02 13:50:39 +0300

    Orders Provider

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,16 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { CategoriesProvider } from "./contexts/CategoriesContext";
 import { ProductsProvider } from "./contexts/ProductsContext";
-
+import { OrdersProvider } from "./contexts/OrdersContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CategoriesProvider>
     <ProductsProvider>
+    <OrdersProvider>
       <Provider store={store}>
         <App />
       </Provider>
+      </OrdersProvider>
     </ProductsProvider>
   </CategoriesProvider>
 );
