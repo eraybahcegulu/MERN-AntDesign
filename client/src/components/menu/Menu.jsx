56134b9d57aebddb2129c3a8edf459a4ce244c5f
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-02 13:49:51 +0300

    Update Menu.jsx

--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -3,22 +3,40 @@ import Categories from "./Categories";
 import { LogoutOutlined, DollarOutlined } from "@ant-design/icons";
 import "./style.css";
 import { Link } from "react-router-dom";
+import { useOrdersData } from "../../contexts/OrdersContext";
+import { useCategoriesData } from "../../contexts/CategoriesContext";
+import { useProductsData } from "../../contexts/ProductsContext";
+import {  Badge } from 'antd';
 
 const Menu = () => {
+  const { orders } = useOrdersData();
+  const { categories } = useCategoriesData();
+  const { products } = useProductsData();
+
   return (
-    <ul className="flex md:flex-col gap-4 text-lg p-4">
+    <ul className="flex md:flex-col gap-6 text-lg p-4">
+
+      <Badge className="text-lg" color='gray' count={products.length}
+      
+      >
       <Products />
+      </Badge>
 
+      <Badge className="text-lg" color='gray' count={categories.length}>
       <Categories />
+      </Badge>
 
-      <Link className="menu-item bg-orange-400 p-2 " to={"/sales"}>
-        <li >
+      
+      <Badge className="menu-item text-lg" color='gray' count={orders.length}>
+      <Link  to={"/sales"}>
+        <li>
           <DollarOutlined />
           <span> SALES </span>
         </li>
       </Link>
+      </Badge>
 
-      <li className="menu-item">
+      <li className="menu-item text-lg">
         <LogoutOutlined />
         <span> LOGOUT</span>
       </li>
