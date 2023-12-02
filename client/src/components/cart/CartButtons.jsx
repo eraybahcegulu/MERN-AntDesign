e16af9c14137af6a8c445c6b30bb685b69e5796f
Eray BAHÇEGÜLÜ <eraybahcegulu@gmail.com>
2023-12-02 10:16:19 +0300

    Update CartButtons.jsx

--- client/src/components/cart/CartButtons.jsx
+++ client/src/components/cart/CartButtons.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, message, Modal, Form, Input, Image, Table } from "antd";
+import { ArrowRightOutlined } from "@ant-design/icons";
 import { useSelector, useDispatch } from "react-redux";
 import { reset } from "../../redux-toolkit/cart/cartSlice";
 import axios from "axios";
@@ -12,21 +13,17 @@ const CartButtons = () => {
   const [isModalOpen, setModalOpen] = useState(false);
 
   const createOrder = async (values) => {
-    const data = { 
-    ...values,
-    cartProducts: cart.cartProducts,
-    subTotal: cart.subTotal,
-    tax: ((cart.subTotal * cart.tax) / 100).toFixed(2),
-    total: (cart.subTotal + (cart.subTotal * cart.tax) / 100).toFixed(2),
-  }
-    console.log(data)
+    const data = {
+      ...values,
+      cartProducts: cart.cartProducts,
+      subTotal: cart.subTotal,
+      tax: ((cart.subTotal * cart.tax) / 100).toFixed(2),
+      total: (cart.subTotal + (cart.subTotal * cart.tax) / 100).toFixed(2),
+    };
+    console.log(data);
     try {
-      await axios.post( process.env.REACT_APP_API_URL + "/api/orders/add", data);
-      message.success(
-        <span>
-          a
-        </span>
-      );
+      await axios.post(process.env.REACT_APP_API_URL + "/api/orders/add", data);
+      message.success(<span>a</span>);
       dispatch(reset());
       createOrderForm.resetFields();
       handleCancel();
@@ -45,10 +42,12 @@ const CartButtons = () => {
 
   const columns = [
     {
-      title: <div className="text-center">Number</div>,
-      dataIndex: 'number',
+      title: <div className="text-center">NO</div>,
+      dataIndex: "number",
       render: (_, record, index) => (
-        <div className="flex flex-row justify-center items-center">{index + 1}</div>
+        <div className="flex flex-row justify-center items-center">
+          {index + 1}
+        </div>
       ),
     },
     {
@@ -96,11 +95,13 @@ const CartButtons = () => {
 
     {
       title: <div className="text-center">Total</div>,
-      dataIndex: 'total',
-      render: (_,record) => {
+      dataIndex: "total",
+      render: (_, record) => {
         const total = record.quantity * record.price;
         return (
-          <div className="flex flex-row justify-center items-center"><strong>{total}$</strong></div>
+          <div className="flex flex-row justify-center items-center">
+            <strong>{total}$</strong>
+          </div>
         );
       },
     },
@@ -108,16 +109,53 @@ const CartButtons = () => {
 
   return (
     <div className="flex flex-col border-b pb-4 gap-4 mt-4">
-      <Button
-        disabled={cart.cartProducts.length === 0}
-        style={{ borderRadius: "0" }}
-        type="primary"
-        size="large"
-        onClick={showCreateOrderModal}
-      >
-        <strong>CONTINUE</strong>
-      </Button>
+      <div className="flex flex-row items-center justify-between gap-1">
+        <Button
+          className="w-full"
+          style={{ borderRadius: "0" }}
+          type="primary"
+          size="large"
+          danger
+          disabled={cart.cartProducts.length === 0}
+          onClick={() => {
+            if (!isModalOpen) {
+              setModalOpen(true);
+              Modal.confirm({
+                content: <span>Are you sure you want to clear cart? </span>,
+                onOk() {
+                  dispatch(reset());
+                  setModalOpen(false);
+                  message.success({
+                    content: (
+                      <span>
+                        <strong>Cart cleared</strong>
+                      </span>
+                    ),
+                    duration: 2,
+                    style: { marginRight: "80%" },
+                  });
+                },
+                onCancel() {
+                  setModalOpen(false);
+                },
+              });
+            }
+          }}
+        >
+          <strong> CLEAR CART </strong>
+        </Button>
 
+        <Button
+          className="w-full"
+          disabled={cart.cartProducts.length === 0}
+          style={{ borderRadius: "0" }}
+          type="primary"
+          size="large"
+          onClick={showCreateOrderModal}
+        >
+          <strong>CONTINUE <ArrowRightOutlined /></strong>
+        </Button>
+      </div>
       <Modal
         style={{ top: 20 }}
         open={IsCreateOrderModal}
@@ -162,38 +200,42 @@ const CartButtons = () => {
             <Input style={{ borderRadius: "0" }} size="large" />
           </Form.Item>
 
+          <div className="flex flex-col ">
+            <Table
+              columns={columns}
+              dataSource={cart.cartProducts}
+              rowKey="_id"
+              size="middle"
+              scroll={{ y: 270 }}
+              pagination={false}
+              className=""
+            />
+            <div className="flex flex-col mt-2">
+              <div className="subtotal flex flex-row justify-between border-b">
+                <span className="text-blue-400">SUBTOTAL</span>
+                <span className="text-blue-400">{cart.subTotal}$</span>
+              </div>
 
+              <div className="tax flex flex-row justify-between border-b mt-2">
+                <span className="text-blue-400">VAT 10%</span>
+                <span className="text-red-500">
+                  +{((cart.subTotal * cart.tax) / 100).toFixed(2)}$
+                </span>
+              </div>
 
-        <div className="flex flex-col ">
-          <Table
-            columns={columns}
-            dataSource={cart.cartProducts}
-            rowKey="_id"
-            size="middle"
-            scroll={{ y: 265 }}
-            pagination={false}
-            className=""
-          />
-          <div  className="flex flex-col mt-2">
-            <div className="subtotal flex flex-row justify-between border-b">
-              <span className="text-blue-400">SUBTOTAL</span>
-              <span className="text-blue-400">{cart.subTotal}$</span>
+              <div className="tax flex flex-row justify-between border-b mt-5">
+                <span className="text-green-600 font-bold text-xl">TOTAL</span>
+                <span className="text-green-600 text-xl">
+                  {(cart.subTotal + (cart.subTotal * cart.tax) / 100).toFixed(
+                    2
+                  )}
+                  $
+                </span>
+              </div>
             </div>
-
-            <div className="tax flex flex-row justify-between border-b mt-2">
-              <span className="text-blue-400">VAT 10%</span>
-              <span className="text-red-500">+{((cart.subTotal * cart.tax) / 100).toFixed(2)}$</span>
-            </div>
-
-            <div className="tax flex flex-row justify-between border-b mt-5">
-              <span className="text-green-600 font-bold text-xl">TOTAL</span>
-              <span className="text-green-600 text-xl">{(cart.subTotal + (cart.subTotal * cart.tax) / 100).toFixed(2)}$</span>
-            </div>
-
           </div>
-        </div>
 
-        <Form.Item className="flex justify-end mb-0">
+          <Form.Item className="flex justify-end mb-0">
             <Button
               style={{ borderRadius: "0" }}
               type="primary"
@@ -206,40 +248,6 @@ const CartButtons = () => {
           </Form.Item>
         </Form>
       </Modal>
-
-      <Button
-        style={{ borderRadius: "0" }}
-        type="primary"
-        size="large"
-        danger
-        disabled={cart.cartProducts.length === 0}
-        onClick={() => {
-          if (!isModalOpen) {
-            setModalOpen(true);
-            Modal.confirm({
-              content: <span>Are you sure you want to clear cart? </span>,
-              onOk() {
-                dispatch(reset());
-                setModalOpen(false);
-                message.success({
-                  content: (
-                    <span>
-                      <strong>Cart cleared</strong>
-                    </span>
-                  ),
-                  duration: 2,
-                  style: { marginRight: "80%" },
-                });
-              },
-              onCancel() {
-                setModalOpen(false);
-              },
-            });
-          }
-        }}
-      >
-        <strong> CLEAR CART </strong>
-      </Button>
     </div>
   );
 };
